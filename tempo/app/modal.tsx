fa63732a53f912507a3cf389e93cf49ae4bb414b
BellaFred <156227097+BellaFred@users.noreply.github.com>
2025-11-15 13:50:04 -0600

    incorporated playlist objects that can be accessed throughout the app

--- tempo/app/modal.tsx
+++ tempo/app/modal.tsx
@@ -1,29 +1,77 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
+import { Link, router  } from 'expo-router';
+import { StyleSheet, TextInput, Button, Alert } from 'react-native';
+import React from 'react';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import {Picker} from '@react-native-picker/picker'
+import {usePlaylists} from '@/context/playlistsContext'
+const TestInputExample = ()=>{
+  const {addPlaylist} = usePlaylists();
+  const [text, onChangeText] = React.useState('new playlist');
+  const [selectedMinBPM, setSelectedMinBPM]=React.useState('70');
+  const [selectedMaxBPM, setSelectedMaxBPM]=React.useState('200');
+  
+  const handleCreatePlaylist = ()=>{
+    addPlaylist(text, selectedMinBPM, selectedMaxBPM);
+    Alert.alert('Playlist Created', `"${text}" has been added!`);
+    router.back();
+  }
 
-export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
+    <ThemedView>
+      <ThemedText type="default">Playlist Name:</ThemedText>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <ThemedText type="default">Minimum BPM:</ThemedText>
+      <Picker
+        selectedValue={selectedMinBPM}
+        onValueChange={(itemValue, itemIndex)=>
+          setSelectedMinBPM(itemValue)
+        }>
+        <Picker.Item label="70" value="70"/> 
+        <Picker.Item label="80" value="80"/> 
+        <Picker.Item label="90" value="90"/> 
+        <Picker.Item label="100" value="100" />
+        <Picker.Item label="110" value="110" />
+        <Picker.Item label="120" value="120" />
+        <Picker.Item label="200" value="200"/> 
+
+      </Picker>
+      <ThemedText type="default">Maximum BPM:</ThemedText>
+      <Picker
+        selectedValue={selectedMaxBPM}
+        onValueChange={(itemValue, itemIndex)=>
+          setSelectedMaxBPM(itemValue)
+        }>
+        <Picker.Item label="70" value="70"/> 
+        <Picker.Item label="80" value="80"/> 
+        <Picker.Item label="90" value="90"/> 
+        <Picker.Item label="100" value="100" />
+        <Picker.Item label="110" value="110" />
+        <Picker.Item label="120" value="120" />
+        <Picker.Item label="200" value="200"/> 
+      </Picker>
+      <Button
+        title="Enter"
+        onPress={handleCreatePlaylist}
+      />
     </ThemedView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: "white",
+    padding: 10,
+    color: "white",
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+   
 });
+
+export default TestInputExample;
